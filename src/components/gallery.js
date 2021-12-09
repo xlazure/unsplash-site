6b6e521f99c2b8241f4e9cef4249522b365bf1a7
xlazure <56347002+xlazure@users.noreply.github.com>
2021-12-09 10:55:11 +0100

    rwd, fullsize img popup

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,29 +1,40 @@
 import styled from "styled-components";
 import { useContext, useState } from "react";
 import { TermContext } from "../context/termContext";
+import { MdOutlinePlace } from "react-icons/md";
 
 export default function Gallery(props) {
   const { setTerm } = useContext(TermContext);
   const [showFullSize, setShowFullSize] = useState(false);
-  const [url, setUrl] = useState();
+  const [imgProps, setImgProps] = useState();
 
   const FullSize = (props) => {
     return (
       <Overlay>
-        <FullImg
-          onClick={() => {
-            setShowFullSize(false);
-          }}
-        >
-          <p>{Image}</p>
-          <img
-            src={props.link}
-            alt="full"
-            onChange={(e) => {
-              console.log("3");
-              console.log(e.target.style.width);
+        <FullImg>
+          <CloseBtn
+            onClick={() => {
+              setShowFullSize(false);
             }}
-          />
+          >
+            Close
+          </CloseBtn>
+          <img src={props?.urls.full} alt="full" />
+          <AuthorBio>
+            <AuthorImg
+              src={props.user?.profile_image.small}
+              alt={props.user.username}
+            />
+            <p>{props.user.username}</p>
+          </AuthorBio>
+          {props.user.location ? (
+            <Place>
+              <MdOutlinePlace size="1.5em" />
+              <p>{props.user.location}</p>
+            </Place>
+          ) : (
+            ""
+          )}
         </FullImg>
       </Overlay>
     );
@@ -31,27 +42,27 @@ export default function Gallery(props) {
 
   return (
     <>
-      {showFullSize ? <FullSize link={url} /> : ""}
+      {showFullSize ? <FullSize {...imgProps} /> : ""}
       <GalleryContainer>
         {props.gallery?.map((el) => {
           return (
             <ContainerImg key={el.id}>
-              <Img
-                src={el?.urls.regular}
-                alt={el?.alt_description}
-                onClick={(e) => {
-                  console.log(e.nativeEvent);
-                  setShowFullSize(true);
-                  setUrl(el?.urls?.full);
-                }}
-              />
-              <AuthorBio>
+              <AuthorBio style={{ position: "absolute" }}>
                 <AuthorImg
                   src={el.user.profile_image.small}
                   alt={el.user.username}
                 />
                 <p>{el.user.username}</p>
               </AuthorBio>
+              <Img
+                src={el?.urls.regular}
+                alt={el?.alt_description}
+                onClick={() => {
+                  setShowFullSize(true);
+                  setImgProps(el);
+                  console.log(imgProps);
+                }}
+              />
               <div>
                 {el.tags?.map((tags, index) => {
                   return (
@@ -80,21 +91,24 @@ const Overlay = styled.div`
 `;
 const FullImg = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
+  flex-direction: column;
   z-index: 103;
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 80vw;
-  height: 80vh;
+  max-width: 80vw;
+
+  max-height: 80vh;
+  /* padding: 3em; */
 
   /* padding: 3em 4em 3em 4em; */
   background: #ffffff;
   img {
-    max-width: 100%;
-    max-height: 100%;
+    width: 100%;
+    height: 100%;
   }
   :hover {
     cursor: zoom-out;
@@ -120,6 +134,32 @@ const ContainerImg = styled.div`
   :nth-of-type(3n + 3) {
     padding-left: 0.4em;
   }
+
+  @media screen and (max-width: 1024px) {
+    width: 50%;
+
+    :nth-of-type(2n + 1) {
+      padding-right: 0.4em;
+    }
+    :nth-of-type(2n + 2) {
+      padding-left: 0.4em;
+    }
+    :nth-of-type(3n + 3) {
+      padding-left: 0;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+
+    :nth-of-type(2n + 1) {
+      padding-right: 0em;
+    }
+    :nth-of-type(2n + 2) {
+      padding-left: 0em;
+    }
+  }
+
   position: relative;
   margin-bottom: 1em;
   /* :nth-of-type(even) {
@@ -159,10 +199,31 @@ const AuthorBio = styled.div`
   align-items: center;
   gap: 0.5em;
   margin-left: 0.3em;
-  position: absolute;
-  bottom: 2.5em;
+  /* position: absolute; */
+  top: 0.2em;
   p {
-    letter-spacing: 0.1rem;
+    font-size: 0.9em;
     filter: drop-shadow(0px 0px 6px #00000086);
+  }
+`;
+const Place = styled.div`
+  display: flex;
+`;
+const CloseBtn = styled.button`
+  position: absolute;
+
+  transition: 0.1s ease;
+  border-radius: 0 !important;
+  border: none;
+  padding: 0.5em 0.7em;
+  right: 0.4em;
+  top: 0.4em;
+  /* position: absolute; */
+  /* top: 1em; */
+  /* left: 1em; */
+  box-shadow: 0px 0px 3px #77777792;
+  :hover {
+    cursor: pointer;
+    box-shadow: 0px 0px 3px #777777;
   }
 `;
